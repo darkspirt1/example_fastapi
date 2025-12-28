f8b34adbd9c0ab42e4225f76de788a020375ee05
darkspirt1 <sahildighe8928@gmail.com>
2025-12-28 16:01:33 +0530

    added commites

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI
 from fastapi.params import Body
 app = FastAPI()
 
-
+# here we define our first route
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
+# here we define a route to get posts
 @app.get("/posts")
 def get_posts():
     return {"posts": ["Post 1", "Post 2", "Post 3"]}
 
-
+# here we define a route to create a post
 @app.post("/createpost")
 def create_post(payload: dict = Body(...)):
     print(payload)
