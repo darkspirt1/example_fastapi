25813882c0c8a1735869ad77f8fad90bbaa7967f
darkspirt1 <sahildighe8928@gmail.com>
2025-12-29 10:22:19 +0530

    Pydantic values

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published : bool = True
+    rating : Optional[int] = None
 # here we define our first route
+
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 # here we define a route to get posts
+
+
 @app.get("/posts")
 def get_posts():
     return {"posts": ["Post 1", "Post 2", "Post 3"]}
 
 # here we define a route to create a post
+
+
 @app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"title": payload['title'], "content": payload['content']}
+def create_post(new_post: Post):
+    print(new_post.rating)
+    return {"data": "new post"}
