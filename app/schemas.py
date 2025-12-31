8cc94c8e5983ceaca3089d08f3fe4415367f7c0c
darkspirt1 <sahildighe8928@gmail.com>
2025-12-31 18:06:41 +0530

    created separate schema file

---
+++ app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class CreatePost(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+
+    class Config:
+        orm_mode = True
