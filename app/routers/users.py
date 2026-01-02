fa71ffdd87cc2da17977f0d91431c87402571d9a
darkspirt1 <sahildighe8928@gmail.com>
2026-01-02 14:58:35 +0530

    separate file to handle users routing

---
+++ app/routers/users.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from app import utilis
+from .. import models, schemas
+from ..database import get_db
+router = APIRouter()
+
+
+
+# here we define a route to get a specific to create user
+
+
+@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # we hash the password - user.password
+    hashed_password = utilis.hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+# here we define a route to get a specific user by id from database
+@router.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if  user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"user with id: {id} was not found")
+    return user
