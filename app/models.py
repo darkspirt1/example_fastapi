567c9d5d394624e6d837e565932eaa2a4a08e4fe
darkspirt1 <sahildighe8928@gmail.com>
2026-01-04 16:20:48 +0530

    created vote table

--- app/models.py
+++ app/models.py
@@ -27,3 +27,11 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+    
